b49a6186e5596757abb88f81fc776fb748c6c33e
Patrick Kirkbride <profplum@me.com>
2022-03-21 12:00:47 -0700

    "added javascript folder for next lab; udpated index to include new javascript"

---
+++ js/main.js
@@ -0,0 +1,18 @@
+{/* <script>
+    const citySubmit = document.querySelector('.citySubmit')
+    const cityField = document.querySelector('.cityField')
+
+    function checkCity() {
+        let userEntry = cityField.value;
+        if (userEntry = "Paris") {
+            alert("Let me take you to the Paris page.")
+        } else if (userEntry = "Berlin") {
+            alert("Let me take you to the Berlin page.")
+        } else if (userEntry = "Kyoto") {
+            alert("The Kyoto site is still under construction. Please select a different city.")
+        } else {
+            alert("We are still working on a page for that city. Please select a different city.")
+        }
+    }
+    citySubmit.addEventListner('click', checkCity)
+</script> */}
