7c193cd41a96687ce21f9a132952ada0ed72b84d
Patrick Kirkbride <profplum@me.com>
2022-03-21 18:02:26 -0700

    "separated scripts into external files to simplify HTML pages"

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
-{/* <script>
-    const citySubmit = document.querySelector('.citySubmit')
-    const cityField = document.querySelector('.cityField')
+const citySubmit = document.querySelector('.citySubmit')
+const cityField = document.querySelector('.cityField')
 
-    function checkCity() {
-        let userEntry = cityField.value;
-        if (userEntry = "Paris") {
-            alert("Let me take you to the Paris page.")
-        } else if (userEntry = "Berlin") {
-            alert("Let me take you to the Berlin page.")
-        } else if (userEntry = "Kyoto") {
-            alert("The Kyoto site is still under construction. Please select a different city.")
-        } else {
-            alert("We are still working on a page for that city. Please select a different city.")
+function checkCity() {
+    let userEntry = cityField.value;
+    if (userEntry == "Paris") {
+        if (confirm("Do you want to go to the Paris page?") == true) {
+                window.location="paris.html"
+            }
+    } else if (userEntry == "Berlin") {
+        if (confirm("Do you want to go to the Berlin page?") == true) {
+                window.location="berlin.html"
         }
+    } else if (userEntry == "Kyoto") {
+        alert("The Kyoto site is still under construction. Please enter a different city.")
+    } else {
+        alert("Are you sure you spelled that right? If so, we are still working on a page for that city. Please select a different city.")
     }
-    citySubmit.addEventListner('click', checkCity)
-</script> */}
+}
+citySubmit.addEventListener('click', checkCity)
