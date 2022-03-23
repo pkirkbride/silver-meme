b7c9053d1e9b558f3aac65273a4bfc4dfcfa743f
Patrick Kirkbride <profplum@me.com>
2022-03-23 15:32:23 -0700

    "leaving this commented out to ensure that the berlin site works correclty without it"

--- js/displayInfo.js
+++ js/displayInfo.js
@@ -1,15 +1,15 @@
-function displayHotel() {
-    document.getElementById("article1").style.display = "revert"
-    document.getElementById("article2").style.display = "none"
-    document.getElementById("article3").style.display = "none"
-}
-function displaySights() {
-    document.getElementById("article2").style.display = "revert"
-    document.getElementById("article1").style.display = "none"
-    document.getElementById("article3").style.display = "none"
-}
-function displayRestaurant() {
-    document.getElementById('article3').style.display = "revert"
-    document.getElementById("article1").style.display = "none"
-    document.getElementById("article2").style.display = "none"
-}
+// function displayHotel() {
+//     document.getElementById("article1").style.display = "revert"
+//     document.getElementById("article2").style.display = "none"
+//     document.getElementById("article3").style.display = "none"
+// }
+// function displaySights() {
+//     document.getElementById("article2").style.display = "revert"
+//     document.getElementById("article1").style.display = "none"
+//     document.getElementById("article3").style.display = "none"
+// }
+// function displayRestaurant() {
+//     document.getElementById('article3').style.display = "revert"
+//     document.getElementById("article1").style.display = "none"
+//     document.getElementById("article2").style.display = "none"
+// }
