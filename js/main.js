a07bc45296de78bc8233843891f2e6c2d670ce96
Patrick Kirkbride <profplum@me.com>
2022-03-23 14:17:29 -0700

    "consolidated js into one file and updated script link on berlin.html to see if it works"

--- js/main.js
+++ js/main.js
@@ -26,3 +26,19 @@ let userName = prompt("What's your name?");
     } else {
         "I respect your privacy."
     }
+
+function displayHotel() {
+    document.getElementById("article1").style.display = "revert"
+    document.getElementById("article2").style.display = "none"
+    document.getElementById("article3").style.display = "none"
+    }
+function displaySights() {
+    document.getElementById("article2").style.display = "revert"
+    document.getElementById("article1").style.display = "none"
+    document.getElementById("article3").style.display = "none"
+    }
+function displayRestaurant() {
+    document.getElementById('article3').style.display = "revert"
+    document.getElementById("article1").style.display = "none"
+    document.getElementById("article2").style.display = "none"
+    }
