0f19682cf36f706445f029cae24d2b8db41ac584
Patrick Kirkbride <profplum@me.com>
2022-03-23 15:57:49 -0700

    "rearranged code in order of appearance on site"

--- js/main.js
+++ js/main.js
@@ -1,31 +1,32 @@
+let viewerName = prompt("What's your name?");
+    if (viewerName != null) {
+        document.getElementById("intro").innerHTML =
+        "Guten tag, " + viewerName + "! Now you know how to say hello in German!";
+    } else {
+        alert("I respect your privacy.");
+
+    }
+
 const citySubmit = document.querySelector('.citySubmit');
 const cityField = document.querySelector('.cityField');
 
 function checkCity() {
     let userEntry = cityField.value;
-    if (userEntry == "Paris") {
+    if (userEntry.toLowerCase() == "paris") {
         if (confirm("Do you want to go to the Paris page?") == true) {
-                window.location="paris.html"
+                window.location="paris.html";
             }
-    } else if (userEntry == "Berlin") {
+    } else if (userEntry.toLowerCase() == "berlin") {
         if (confirm("Do you want to go to the Berlin page?") == true) {
-                window.location="berlin.html"
+                window.location="berlin.html";
         }
-    } else if (userEntry == "Kyoto") {
-        alert("The Kyoto site is still under construction. Please enter a different city.")
+    } else if (userEntry.toLowerCase() == "kyoto") {
+        alert("The Kyoto site is still under construction. Please enter a different city.");
     } else {
-        alert("Are you sure you spelled that right? If so, we are still working on a page for that city. Please select a different city.")
+        alert("Are you sure you spelled that right? If so, we are still working on a page for that city. Please select a different city.");
     }
 }
 citySubmit.addEventListener('click', checkCity)
-
-let userName = prompt("What's your name?");
-    if (userName != null) {
-        document.getElementById("intro").innerHTML =
-        "Guten tag, " + userName + "! Now you know how to say hello in German!";
-    } else {
-        "I respect your privacy."
-    }
 
 function displayHotel() {
     document.getElementById("article1").style.display = "revert"
