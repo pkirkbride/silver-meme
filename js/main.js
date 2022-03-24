a2f5fc00d23da0ca7b1ce7615d708b73b4893793
Patrick Kirkbride <profplum@me.com>
2022-03-24 11:54:40 -0700

    "changed operand from undefined back to null in name prompt field. It didn't seem to make a difference."

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const cityField = document.querySelector('.cityField');
 
 function learnGerman() {
     let viewerName = prompt("What's your name?", "Traveler");
-        if (viewerName != null) {
+        if (viewerName !== null) {
             document.getElementById("intro").innerHTML = "Guten tag, <strong>" + viewerName + "</strong>! Now you know how to say hello in German!" 
         } else {
             alert("I respect your privacy.");
